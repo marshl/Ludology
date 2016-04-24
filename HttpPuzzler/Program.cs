0d13819bfe95e35c893ecb17e6e3037fe8f1b681
marshl <marshl@live.com.au>
2016-04-24 13:13:53 +1000

    Added a basic Rpc client to send the initial game registration

--- HttpPuzzler/Program.cs
+++ HttpPuzzler/Program.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Collections.Generic;
 using AustinHarris.JsonRpc;
+using System.Threading;
+using System.Reactive.Concurrency;
+using System.Diagnostics;
 
 namespace HttpPuzzler
 {
@@ -12,6 +15,41 @@ namespace HttpPuzzler
 
         static int Main(string[] args)
         {
+            /*JsonRpcClient
+            Uri remoteUri = ;
+            //var client = AustinHarris.JsonRpc.
+            var rpcResultHandler = new AsyncCallback(_ => Console.WriteLine(((JsonRpcStateAsync)_).Result));
+            var async = new JsonRpcStateAsync(rpcResultHandler, null);
+            async.JsonRpc = "{'method':'add','params':[1,2],'id':1}";
+            JsonRpcProcessor.Process(async);*/
+            {
+                AutoResetEvent are = new AutoResetEvent(false);
+                var rpc = new JsonRpcClient(new Uri("http://localhost:49718/json.rpc"));
+                string method = "RegistrationService.Register";
+                JsonResponse<Newtonsoft.Json.Linq.JObject> responseResult = null;
+                var myObs = rpc.Invoke<Newtonsoft.Json.Linq.JObject>(method, null, Scheduler.TaskPool);
+
+                myObs.Subscribe(
+                    onNext: x =>
+                    {
+                        responseResult = x;
+                        are.Set();
+                    },
+                    onError: x =>
+                    {
+                        are.Set();
+                    },
+                    onCompleted: () => { are.Set(); }
+                    );
+
+                are.WaitOne();
+                var res = responseResult?.Result;
+            }
+            /*var myAsync = new JsonRpcStateAsync();
+            myAsync.JsonRpc = "{method:'yourMethod',params:[],id:0}";
+            var myCusomContext = new { }; // ..Anything..; But be consistant. :)
+            JsonRpcProcessor.Process(myAsync, myCustomContext);*/
+
             /*var solver = new TicTacToe.TicTacToeSolver(new TicTacToe.TicTacToeBoard(new string[] {
                 "O", "X", "O",
                 null, null, null,
